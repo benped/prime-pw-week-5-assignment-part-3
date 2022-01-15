5fe8c4f6a78d1fdbbdd7a797eab6559ab0742251
Ben Pedrick <bpedrick@gmail.com>
2022-01-15 11:24:25 -0600

    addToCollection written

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,18 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title1, artist1, yearPublished1) {
+  console.log('Inside addToCollection Function');
+  let record = {
+    title: title1,
+    artist: artist1,
+    yearPublished: yearPublished1,
+  };
+  collection += record;
+  return record;
+}
+
+addToCollection('Let it Be','The Beatles','1969');
+
+console.log
