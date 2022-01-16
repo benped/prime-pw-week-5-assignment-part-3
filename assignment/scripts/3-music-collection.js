af944bedc7fe7150a144fe46932713def940ee8d
Ben Pedrick <bpedrick@gmail.com>
2022-01-16 10:52:57 -0600

    fixed small undefined bug in console and added html questions

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -16,7 +16,7 @@ function addToCollection(title, artist, yearPublished, ...tracks) {
 }
 // End addToCollection function
 
-function trackBuilder(name, duration){
+function trackBuilder(name, duration) {
   let track = {
     trackName: name,
     duration: duration,
@@ -26,12 +26,15 @@ function trackBuilder(name, duration){
 
 // console.log('Trackbuilder test:',trackBuilder('Test','Time'));
 
-
-
 function showCollection(array) {
   console.log(array.length);
-  for ( let i of array) {
+  for (let i of array) {
     console.log(`${i.title} by ${i.artist} published in ${i.yearPublished}`);
+    let lead = 1;
+    for (let ii of i.tracks){
+      console.log(`${lead}. ${ii.trackName} : ${ii.duration}`)
+      lead++;
+    }
   }
 }
 
@@ -39,7 +42,7 @@ function showCollection(array) {
 function findByArtist(artist) {
   let artistList = [];
   for (let i of collection) {
-    if (i.artist === artist){
+    if (i.artist === artist) {
       // console.log(i.artist);
       artistList.push(i);
     }
@@ -49,77 +52,77 @@ function findByArtist(artist) {
 
 
 
-function search(sInput){
+function search(sInput) {
 
   let searchArray = [];
 
-  if (sInput == null){
+  if (sInput == null) {
     return collection;
   }
 
-  for (let i of collection){
+  for (let i of collection) {
     let trackMatch = false;
     // console.log(i.tracks);
-    for (let ii of i.tracks){
+    for (let ii of i.tracks) {
       // console.log(ii);
-      if (ii.trackName === sInput.trackName){
+      if (ii.trackName === sInput.trackName) {
         trackMatch = true;
       }
     }
     if (i.artist === sInput.artist || i.yearPublished === sInput.yearPublished ||
-    trackMatch) {
+      trackMatch) {
       searchArray.push(i);
     }
 
-    }
-    return searchArray;
   }
+  return searchArray;
+} //end search function
 
 
 
-  let searchObject0 = {
-    artist: 'Ray Charles',
-    yearPublished: '1957',
-  }
+let searchObject0 = {
+  artist: 'Ray Charles',
+  yearPublished: '1957',
+}
 
-  let searchObject1 = {
-    artist: 'Daft Punk',
-    yearPublished: '2001',
-    trackName: 'Da Funk',
-  }
+let searchObject1 = {
+  artist: 'Daft Punk',
+  yearPublished: '2001',
+  trackName: 'Da Funk',
+}
 
-  let searchObject2 = {
-    trackName: 'Love Again'
-  }
+let searchObject2 = {
+  trackName: 'Love Again'
+}
 
-  let searchObject3 = {
-    trackName: 'Ode to the Mets',
-  }
+let searchObject3 = {
+  trackName: 'Ode to the Mets',
+}
 
 
 
-addToCollection('Let it Be','The Beatles','1970');
+addToCollection('Let it Be', 'The Beatles', '1970');
 
 console.log(collection);
 
-addToCollection('Homework','Daft Punk','2001',trackBuilder('Da Funk','5:28'),trackBuilder('Around the World','7:09'));
+addToCollection('Homework', 'Daft Punk', '2001', trackBuilder('Da Funk', '5:28'),trackBuilder('Around the World', '7:09'));
 // console.log(collection[1]);
 // rest parameter test
-addToCollection('Bubba','Kaytranada','2019');
-addToCollection('Random Access Memories','Daft Punk','2013');
-addToCollection('The New Abnormal','The Strokes','2020', trackBuilder('Ode to the Mets','5:51'));
-addToCollection('Future Nostalgia','Dua Lipa','2020',trackBuilder("Don't Start Now",'3:03'),trackBuilder('Love Again','4:18'),trackBuilder('Levitating','3:23'));
+addToCollection('Bubba', 'Kaytranada', '2019');
+addToCollection('Random Access Memories', 'Daft Punk', '2013');
+addToCollection('The New Abnormal', 'The Strokes', '2020', trackBuilder('Ode to the Mets', '5:51'));
+addToCollection('Future Nostalgia', 'Dua Lipa', '2020', trackBuilder("Don't Start Now", '3:03'), trackBuilder('Love Again', '4:18'), trackBuilder('Levitating', '3:23'));
 
 console.log(collection);
 
-console.log(showCollection(collection));
+showCollection(collection);
 
 // findByArtistTest
 console.log('Looking for anything by Daft Punk', findByArtist('Daft Punk'));
 console.log('Looking for anything by Metallica', findByArtist('Metallica'));
 
 console.log('Searching collection for Ray Charles 1957', search(searchObject0));
-console.log('Searching collection for Daft Punk 2001',search(searchObject1));
-console.log('Searching for nothing in particular',search());
+console.log('Searching collection for Daft Punk 2001', search(searchObject1));
+console.log('Searching for nothing in particular', search());
 console.log("I'm looking for an album with the song Ode to the Mets", search(searchObject3));
 console.log("I'm looking for an album with the song Love Again", search(searchObject2));
